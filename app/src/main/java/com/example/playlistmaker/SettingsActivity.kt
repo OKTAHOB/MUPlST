48eab04b8c692b5402ba5d02c5d89d92a9e48662
OKTAHOB <okahob@yandex.kz>
2025-04-27 23:46:03 +0500

    9_settings_menu_links_done

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,9 +1,12 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -16,6 +19,43 @@ class SettingsActivity : AppCompatActivity() {
         backArrow.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+        val btnShare = findViewById<MaterialTextView>(R.id.btn_share)
+        btnShare.setOnClickListener {
 
+            val shareText = getString(R.string.course_url)
+
+            val sendIntent = Intent(Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, shareText)
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, getString(R.string.share_via))
+            startActivity(shareIntent)
+        }
+
+        val btnContactSupport = findViewById<MaterialTextView>(R.id.btn_contact_support)
+
+        btnContactSupport.setOnClickListener {
+            val email = getString(R.string.sup_mail)
+            val subject = getString(R.string.message_to_sup_h)
+            val body = getString(R.string.message_to_sup)
+
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                putExtra(Intent.EXTRA_SUBJECT, subject)
+                putExtra(Intent.EXTRA_TEXT, body)
+            }
+            startActivity(intent)
+        }
+        val btnTerms = findViewById<MaterialTextView>(R.id.btn_terms_of_use)
+
+        btnTerms.setOnClickListener {
+
+            val termsUrl = getString(R.string.terms_url)
+
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(termsUrl))
+            startActivity(intent)
+        }
     }
 }
