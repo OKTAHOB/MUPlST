e34ad5293c5ffe2315a4ad67c2f3ecf3dfc9a49f
OKTAHOB <okahob@yandex.kz>
2025-08-06 15:19:30 +0500

    19_sprint_BUT_SEARCH

--- app/src/main/java/com/example/playlistmaker/features/media/presentation/ui/MediaLibraryPagerAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/features/media/presentation/ui/MediaLibraryPagerAdapter.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class MediaLibraryPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+class MediaLibraryPagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
     override fun getItemCount(): Int = 2
     override fun createFragment(position: Int): Fragment =
         when (position) {
